b020da92045af335563c19dbc83099e1786833cb
George Smyrnaios <smyrgeorge@gmail.com>
2024-06-29 13:38:40 +0200

    Optimized the tx holder (reduced the amount of locks)

---
+++ src/nativeMain/kotlin/io/github/smyrgeorge/sqlx4k/driver/impl/extensions.kt
@@ -0,0 +1,34 @@
+package io.github.smyrgeorge.sqlx4k.driver.impl
+
+import io.github.smyrgeorge.sqlx4k.driver.Driver.Companion.idx
+import io.github.smyrgeorge.sqlx4k.driver.Driver.Companion.map
+import io.github.smyrgeorge.sqlx4k.driver.Driver.Companion.mutexMap
+import io.github.smyrgeorge.sqlx4k.driver.Transaction.Companion.arr
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.withLock
+import librust_lib.Sqlx4kResult
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.suspendCoroutine
+
+@OptIn(ExperimentalForeignApi::class)
+suspend inline fun call(crossinline f: (idx: ULong) -> Unit): CPointer<Sqlx4kResult>? =
+    suspendCoroutine { c: Continuation<CPointer<Sqlx4kResult>?> ->
+        runBlocking {
+            // The [runBlocking] it totally fine at this level.
+            // We only lock for a very short period of time, just to store in the HashMap.
+            val idx = idx()
+            mutexMap.withLock { map[idx] = c } // Store the [Continuation] object to the HashMap.
+            f(idx)
+        }
+    }
+
+@OptIn(ExperimentalForeignApi::class)
+suspend inline fun call(id: Int, crossinline f: () -> Unit): CPointer<Sqlx4kResult>? =
+    suspendCoroutine { c: Continuation<CPointer<Sqlx4kResult>?> ->
+        runBlocking {
+            arr[id] = c
+            f()
+        }
+    }
