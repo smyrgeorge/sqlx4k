b4f688ccfd49a34a8b96d88abc86bc64945310b0
George Smyrnaios <smyrgeorge@gmail.com>
2024-08-29 00:27:47 +0200

    Modified MultiplatformConventions.

--- build-logic/multiplatform-convention/src/main/kotlin/io/github/smyrgeorge/sqlx4k/multiplatform/MultiplatformConventions.kt
+++ build-logic/multiplatform-convention/src/main/kotlin/io/github/smyrgeorge/sqlx4k/multiplatform/MultiplatformConventions.kt
@@ -3,10 +3,10 @@ package io.github.smyrgeorge.sqlx4k.multiplatform
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
 import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import java.io.File
 import java.lang.System.getenv
@@ -63,8 +63,43 @@ class MultiplatformConventions : Plugin<Project> {
 
 
     override fun apply(project: Project) {
+        val targets = project.targets()
+        project.plugins.apply("org.jetbrains.kotlin.multiplatform")
+        project.extensions.configure<KotlinMultiplatformExtension> {
+            val availableTargets = mapOf(
+                Pair("iosArm64") { iosArm64 { rust("aarch64-apple-ios", !os.isMacOsX) } },
+                Pair("androidNativeX64") { androidNativeX64 { rust("x86_64-linux-android", true) } },
+                Pair("androidNativeArm64") { androidNativeArm64 { rust("aarch64-linux-android", true) } },
+                Pair("macosArm64") { macosArm64 { rust("aarch64-apple-darwin", !os.isMacOsX) } },
+                Pair("macosX64") { macosX64 { rust("x86_64-apple-darwin", !os.isMacOsX) } },
+                Pair("linuxArm64") { linuxArm64 { rust("aarch64-unknown-linux-gnu", !os.isLinux || !arch.isArm64) } },
+                Pair("linuxX64") { linuxX64 { rust("x86_64-unknown-linux-gnu", !os.isLinux) } },
+                Pair("mingwX64") { mingwX64 { rust("x86_64-pc-windows-gnu", true) } },
+            )
 
-        val chosenTargets = (project.properties["targets"] as? String)?.let {
+            targets.forEach {
+                println("Enabling target $it")
+                availableTargets[it]?.invoke()
+            }
+
+            applyDefaultHierarchyTemplate()
+        }
+    }
+
+    /**
+     * Returns a list of target strings for the project.
+     *
+     * If the "targets" property is present and its value is "all", the method returns a fixed list
+     * of targets. Otherwise, it splits the value of the "targets" property by commas and trims each
+     * target string before returning the list.
+     *
+     * If the "targets" property is not present,
+     * the method returns a default list containing only the target "macosArm64".
+     *
+     * @return a list of target strings for the project
+     */
+    private fun Project.targets(): List<String> =
+        (properties["targets"] as? String)?.let {
             when (it) {
                 "all" -> listOf(
                     "iosArm64",
@@ -81,28 +116,6 @@ class MultiplatformConventions : Plugin<Project> {
             }
         } ?: listOf("macosArm64") // For local development.
 
-        project.plugins.apply("org.jetbrains.kotlin.multiplatform")
-        (project.kotlinExtension as KotlinMultiplatformExtension).apply {
-            val availableTargets = mapOf(
-                Pair("iosArm64") { iosArm64 { rust("aarch64-apple-ios", !os.isMacOsX) } },
-                Pair("androidNativeX64") { androidNativeX64 { rust("x86_64-linux-android", true) } },
-                Pair("androidNativeArm64") { androidNativeArm64 { rust("aarch64-linux-android", true) } },
-                Pair("macosArm64") { macosArm64 { rust("aarch64-apple-darwin", !os.isMacOsX) } },
-                Pair("macosX64") { macosX64 { rust("x86_64-apple-darwin", !os.isMacOsX) } },
-                Pair("linuxArm64") { linuxArm64 { rust("aarch64-unknown-linux-gnu", !os.isLinux || !arch.isArm64) } },
-                Pair("linuxX64") { linuxX64 { rust("x86_64-unknown-linux-gnu", !os.isLinux) } },
-                Pair("mingwX64") { mingwX64 { rust("x86_64-pc-windows-gnu", true) } },
-            )
-
-            chosenTargets.forEach {
-                println("Enabling target $it")
-                availableTargets[it]?.invoke()
-            }
-
-            applyDefaultHierarchyTemplate()
-        }
-    }
-
     private fun KotlinNativeTarget.rust(target: String, useCross: Boolean = false) {
         val tasks = project.tasks
         fun file(path: String) = project.projectDir.resolve(path)
@@ -113,11 +126,7 @@ class MultiplatformConventions : Plugin<Project> {
                     if (useCross) {
                         project.exec {
                             executable = cargo
-                            args(
-                                "install",
-                                "cross",
-                                "--git", "https://github.com/cross-rs/cross"
-                            )
+                            args("install", "cross", "--git", "https://github.com/cross-rs/cross")
                         }
                     }
 
