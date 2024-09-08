703d09b75b8c2b22c0489babc80e998d70878e15
George Smyrnaios <smyrgeorge@gmail.com>
2024-09-08 16:38:43 +0200

    Updated publish script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "io.github.smyrgeorge"
-version = "0.9.4"
+version = "0.9.5"
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env sh
 
+./gradlew clean
+
+./gradlew :sqlx4k-mysql:build -Ptargets=iosArm64
+./gradlew :sqlx4k-mysql:build -Ptargets=androidNativeX64
+./gradlew :sqlx4k-mysql:build -Ptargets=androidNativeArm64
+./gradlew :sqlx4k-mysql:build -Ptargets=macosArm64
+./gradlew :sqlx4k-mysql:build -Ptargets=macosX64
+./gradlew :sqlx4k-mysql:build -Ptargets=linuxArm64
+./gradlew :sqlx4k-mysql:build -Ptargets=linuxX64
+
+./gradlew :sqlx4k-postgres:build -Ptargets=iosArm64
+./gradlew :sqlx4k-postgres:build -Ptargets=androidNativeX64
+./gradlew :sqlx4k-postgres:build -Ptargets=androidNativeArm64
+./gradlew :sqlx4k-postgres:build -Ptargets=macosArm64
+./gradlew :sqlx4k-postgres:build -Ptargets=macosX64
+./gradlew :sqlx4k-postgres:build -Ptargets=linuxArm64
+./gradlew :sqlx4k-postgres:build -Ptargets=linuxX64
+
+./gradlew :sqlx4k-sqlite:build -Ptargets=iosArm64
+./gradlew :sqlx4k-sqlite:build -Ptargets=androidNativeX64
+./gradlew :sqlx4k-sqlite:build -Ptargets=androidNativeArm64
+./gradlew :sqlx4k-sqlite:build -Ptargets=macosArm64
+./gradlew :sqlx4k-sqlite:build -Ptargets=macosX64
+./gradlew :sqlx4k-sqlite:build -Ptargets=linuxArm64
+./gradlew :sqlx4k-sqlite:build -Ptargets=linuxX64
+
 cat local.properties >> gradle.properties
-
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=iosArm64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=androidNativeX64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=androidNativeArm64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=macosArm64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=macosX64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=linuxArm64
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=linuxX64
-
-
+./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=all
 git checkout .
