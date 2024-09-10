319f43af04962c374a81848aa4cef68c1775c1a2
George Smyrnaios <smyrgeorge@gmail.com>
2024-09-10 10:43:51 +0200

    Revert "Simplified build setup."
    This reverts commit bb9bb2419fbb9ea07762f5fa7702f18d18ac2889.

--- sqlx4k-sqlite/examples/build.gradle.kts
+++ sqlx4k-sqlite/examples/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
@@ -11,41 +9,8 @@ repositories {
 group = rootProject.group
 version = rootProject.version
 
-private fun Project.targets(): List<String> =
-    (properties["targets"] as? String)?.let {
-        when (it) {
-            "all" -> listOf(
-                "iosArm64",
-                "androidNativeX64",
-                "androidNativeArm64",
-                "macosArm64",
-                "macosX64",
-                "linuxArm64",
-                "linuxX64",
-//                "mingwX64"
-            )
-
-            else -> it.split(",").map { t -> t.trim() }
-        }
-    } ?: listOf("macosArm64") // For local development.
-
 kotlin {
-    val targets = project.targets()
-    val availableTargets: Map<String, () -> KotlinNativeTarget> = mapOf(
-        Pair("iosArm64") { iosArm64 { binaries { executable() } } },
-        Pair("androidNativeX64") { androidNativeX64 { binaries { executable() } } },
-        Pair("androidNativeArm64") { androidNativeArm64 { binaries { executable() } } },
-        Pair("macosArm64") { macosArm64 { binaries { executable() } } },
-        Pair("macosX64") { macosX64 { binaries { executable() } } },
-        Pair("linuxArm64") { linuxArm64 { binaries { executable() } } },
-        Pair("linuxX64") { linuxX64 { binaries { executable() } } },
-        Pair("mingwX64") { mingwX64 { binaries { executable() } } },
-    )
-
-    targets.forEach {
-        println("Enabling target $it")
-        availableTargets[it]?.invoke()
-    }
+    macosArm64 { binaries { executable() } }
 
     applyDefaultHierarchyTemplate()
     sourceSets {
