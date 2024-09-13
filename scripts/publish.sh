ddad1eb76a6987297dcecfd6cd2df16af741817f
George Smyrnaios <smyrgeorge@gmail.com>
2024-09-13 13:11:38 +0200

    Modified publish.sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 cat local.properties >> gradle.properties
-./gradlew publishAllPublicationsToMavenCentralRepository -Ptargets=all
+./gradlew clean publishAllPublicationsToMavenCentralRepository -Ptargets=all
 git checkout .
