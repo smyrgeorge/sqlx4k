903daf32d1bc7d64c30e9b6a05e457a94c3e1871
George Smyrnaios <smyrgeorge@gmail.com>
2024-09-14 13:30:37 +0200

    Add Dokka plugin to subprojects
    Include Dokka plugin in build configuration for documentation generation. Updated subprojects to apply the Dokka plugin, enabling the use of the dokkaHtmlMultiModule task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,15 @@ group = "io.github.smyrgeorge"
 version = "0.16.0"
 
 plugins {
+    alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
 repositories {
     mavenCentral()
+}
+
+subprojects {
+    // Run with ./gradlew :dokkaHtmlMultiModule
+    apply(plugin = "org.jetbrains.dokka")
 }
