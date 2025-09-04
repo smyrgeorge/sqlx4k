ddd55ef69fa2a8d7a56c649d25962b082782f440
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2025-09-04 22:38:25 +0200

    Refactor `TableProcessor` and `RepositoryProcessor` to centralize type names in `TypeNames`, streamline annotation processing, and reduce redundant code.

---
+++ sqlx4k-codegen/src/jvmMain/kotlin/io/github/smyrgeorge/sqlx4k/processor/TypeNames.kt
@@ -0,0 +1,23 @@
+package io.github.smyrgeorge.sqlx4k.processor
+
+/**
+ * Centralized fully qualified names used by codegen processors to avoid magic strings.
+ */
+object TypeNames {
+    // Annotations
+    const val REPOSITORY_ANNOTATION = "io.github.smyrgeorge.sqlx4k.annotation.Repository"
+    const val TABLE_ANNOTATION = "io.github.smyrgeorge.sqlx4k.annotation.Table"
+    const val ID_ANNOTATION = "io.github.smyrgeorge.sqlx4k.annotation.Id"
+    const val COLUMN_ANNOTATION = "io.github.smyrgeorge.sqlx4k.annotation.Column"
+
+    // Core interfaces/classes
+    const val CRUD_REPOSITORY = "io.github.smyrgeorge.sqlx4k.CrudRepository"
+    const val QUERY_EXECUTOR = "io.github.smyrgeorge.sqlx4k.QueryExecutor"
+    const val STATEMENT = "io.github.smyrgeorge.sqlx4k.Statement"
+
+    // Kotlin stdlib
+    const val KOTLIN_RESULT = "kotlin.Result"
+    const val KOTLIN_LIST = "kotlin.collections.List"
+    const val KOTLIN_LONG = "kotlin.Long"
+    const val KOTLIN_INT = "kotlin.Int"
+}
