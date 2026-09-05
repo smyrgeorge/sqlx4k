39f7fb79a34ecb19e6f2e7f0e4e14147c21b7fcd
Yorgos S. <smyrgeorge@gmail.com>
2026-09-05 18:58:40 +0200

    Include `sqlx4k-codegen-test` and `sqlx4k-sqlite-cipher` modules in Dokka configuration.

--- dokka/build.gradle.kts
+++ dokka/build.gradle.kts
@@ -6,10 +6,12 @@ dependencies {
     dokka(project(":sqlx4k"))
     dokka(project(":sqlx4k-arrow"))
     dokka(project(":sqlx4k-codegen"))
+    dokka(project(":sqlx4k-codegen-test"))
     dokka(project(":sqlx4k-mysql"))
     dokka(project(":sqlx4k-postgres"))
     dokka(project(":sqlx4k-postgres-pgmq"))
     dokka(project(":sqlx4k-sqlite"))
+    dokka(project(":sqlx4k-sqlite-cipher"))
 }
 
 dokka {
